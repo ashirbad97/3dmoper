55274ea40fcd530cc0e4534a9a4d64d811375c63
Ashirbad Samantaray <samantaray.ashirbad@gmail.com>
2022-01-12 15:27:51 +0530

    Complete API Call to check if the user exist in server

--- Assets/Scripts/PreMainMenu.cs
+++ Assets/Scripts/PreMainMenu.cs
@@ -9,6 +9,7 @@ using UnityEngine.Networking;
 
 public class PreMainMenu : MonoBehaviour {
 
+    string responseOkMsg = "Subject Found";
     public static string playerUID;
     public TMP_InputField subjectUID; 
     const string apiEndpoint = "http://192.168.225.193:3000/api/checkUser";
@@ -16,7 +17,6 @@ public class PreMainMenu : MonoBehaviour {
     {
         playerUID = subjectUID.text;
         StartCoroutine(ProcessRequest(apiEndpoint, playerUID));
-        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
     public void QuitGame()
     {
@@ -33,9 +33,31 @@ public class PreMainMenu : MonoBehaviour {
         {
             yield return request.SendWebRequest();
             if (request.isNetworkError || request.isHttpError)
-                Debug.Log("Error is : "+ request.error);
+            {
+                Debug.Log("Error is : " + request.error);
+                Debug.Log("User Not Found, add User and try again");
+            }
             else
+            {
                 Debug.Log(request.downloadHandler.text);
+                Debug.Log(request.responseCode);
+                if(request.responseCode == 200)
+                {
+                    if(request.downloadHandler.text == responseOkMsg)
+                    {
+                        Debug.Log("User Found, Starting Trials");
+                        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+                    }
+                    else
+                    {
+                        Debug.Log("Status 200, But User Not Found");
+                    }                   
+                }
+                else
+                {
+                    Debug.Log("User Not Found, add User and try again");
+                }
+            }           
         }
     }
 }
